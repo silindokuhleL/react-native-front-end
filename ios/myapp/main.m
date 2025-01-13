a98d8c7e459d89837b2b023ec855171fef461ab2
silindokuhleL <silindokuhlelmapiyeye@gmail.com>
2025-01-13 18:16:51 +0200

    Initial commit for React Native app

---
+++ ios/myapp/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autorelease pool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
+

30313e584b5c21fe7fc268c14d5d6847509b2c86
silindokuhleL <silindokuhlelmapiyeye@gmail.com>
2025-02-03 06:15:29 +0200

    added categories and services hook

--- app/(tabs)/manage-services.tsx
+++ app/(tabs)/manage-services.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ThemedText } from '../../components/ThemedText';
+import { ThemedText } from '@/components/ThemedText';
 
 export default function ManageServicesScreen() {
   return (
